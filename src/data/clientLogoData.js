4ce961ba1d4771648c3fd7bce140cc36c0bd1808
wakar ahamed <wakar.a@dsp.com.sg>
2025-05-25 18:00:36 +0530

    added client logo section

--- src/data/clientLogoData.js
+++ src/data/clientLogoData.js
@@ -5,19 +5,19 @@ const clientLogoData = [
   },
   {
     id: 2,
-    imageUrl: "/images/ClientLogosSection/Picture1.png",
+    imageUrl: "/images/ClientLogosSection/Picture2.png",
   },
   {
     id: 3,
-    imageUrl: "/images/ClientLogosSection/Picture1.png",
+    imageUrl: "/images/ClientLogosSection/Picture3.png",
   },
   {
     id: 4,
-    imageUrl: "/images/ClientLogosSection/Picture1.png",
+    imageUrl: "/images/ClientLogosSection/Picture4.png",
   },
   {
     id: 5,
-    imageUrl: "/images/ClientLogosSection/Picture1.png",
+    imageUrl: "/images/ClientLogosSection/Picture5.png",
   },
 ];
 
--- src/sections/ClientLogosSection.jsx
+++ src/sections/ClientLogosSection.jsx
@@ -1,5 +1,12 @@
+import clientLogoData from "../data/clientLogoData";
 const ClientLogosSection = () => {
-  return <div>ClientLogosSection</div>;
+  return (
+    <div>
+      {clientLogoData.map((item) => {
+        return <img src={item.imageUrl} />;
+      })}
+    </div>
+  );
 };
 
 export default ClientLogosSection;
